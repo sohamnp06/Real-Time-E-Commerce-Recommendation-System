e2d5db42d57c1de54d9ee8d303dc6c343a1c24fd
sohamnp06 <sohampashilkar84@gmail.com>
2026-03-07 22:40:31 +0530

    flask setup

---
+++ app.py
@@ -0,0 +1,149 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from pipeline.recommendation_engine import recommend_products
+from database import get_connection
+
+app = Flask(__name__)
+
+# -----------------------------
+# HOME PAGE
+# -----------------------------
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+# -----------------------------
+# SIGNUP PAGE
+# -----------------------------
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        country = request.form["country"]
+        state = request.form["state"]
+        city = request.form["city"]
+        postal = request.form["postal"]
+
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        INSERT INTO customers(name,email,password,country,state,city,postal_code)
+        VALUES(%s,%s,%s,%s,%s,%s,%s)
+        """,(name,email,password,country,state,city,postal))
+
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for("login"))
+
+    return render_template("signup.html")
+
+
+# -----------------------------
+# LOGIN PAGE
+# -----------------------------
+@app.route("/login", methods=["GET","POST"])
+def login():
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        SELECT customer_id FROM customers
+        WHERE email=%s AND password=%s
+        """,(email,password))
+
+        user = cursor.fetchone()
+
+        conn.close()
+
+        if user:
+            return redirect(url_for("products"))
+
+        else:
+            return "Invalid Login"
+
+    return render_template("login.html")
+
+
+# -----------------------------
+# PRODUCT PAGE
+# -----------------------------
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+
+# -----------------------------
+# RECOMMENDATION API
+# -----------------------------
+@app.route("/recommend", methods=["POST"])
+def recommend():
+
+    data = request.json
+
+    customer_vector = data["customer_vector"]
+    product_id = data["product_id"]
+
+    recommendations = recommend_products(customer_vector, product_id)
+
+    return jsonify({
+        "recommended_products": list(recommendations)
+    })
+
+
+# -----------------------------
+# CART PAGE
+# -----------------------------
+@app.route("/cart")
+def cart():
+    return render_template("cart.html")
+
+
+# -----------------------------
+# BILLING PAGE
+# -----------------------------
+@app.route("/billing")
+def billing():
+    return render_template("billing.html")
+
+
+# -----------------------------
+# PLACE ORDER
+# -----------------------------
+@app.route("/place_order", methods=["POST"])
+def place_order():
+
+    customer_id = request.form["customer_id"]
+    total_amount = request.form["total_amount"]
+    payment_method = request.form["payment_method"]
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO orders(customer_id,total_amount,payment_method)
+    VALUES(%s,%s,%s)
+    """,(customer_id,total_amount,payment_method))
+
+    conn.commit()
+    conn.close()
+
+    return render_template("order_success.html")
+
+
+# -----------------------------
+# RUN APP
+# -----------------------------
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ pipeline/recommendation_engine.py
@@ -0,0 +1,47 @@
+print("hello")
+
+import pickle
+import pandas as pd
+import numpy as np
+
+kmeans = pickle.load(open("models\customer_kmeans.pkl", "rb"))
+knn = pickle.load(open("models\product_recommender_model.pkl", "rb"))
+product_similarity = pickle.load(open("models\product_similarity_matrix.pkl", "rb"))
+
+data = pd.read_csv("data/amazonSale.csv")
+
+
+def get_customer_cluster(customer_features):
+    cluster = kmeans.predict([customer_features])
+    return cluster
+
+
+def get_similar_customers(customer_vector):
+    distances, indices = knn.kneighbors([customer_vector], n_neighbors=5)
+    return indices
+
+
+def get_similar_products(product_id):
+
+    product_index = data[data['Product ID'] == product_id].index[0]
+
+    similarity_scores = list(enumerate(product_similarity[product_index]))
+
+    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+
+    similar_products = similarity_scores[1:10]
+
+    product_indices = [i[0] for i in similar_products]
+
+    return data.iloc[product_indices]['Product Name'].values
+
+
+def recommend_products(customer_vector, product_id):
+
+    cluster = get_customer_cluster(customer_vector)
+
+    similar_customers = get_similar_customers(customer_vector)
+
+    product_recommendations = get_similar_products(product_id)
+
+    return product_recommendations
